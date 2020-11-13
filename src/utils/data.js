624d026bc2089ac4b0532184a340003aa7801a6a
Elvira Fuente <elvira.fuente@ultimasoftware.com>
2020-11-13 16:50:38 +0100

    add radius and position keys to data json

---
+++ src/utils/data.js
@@ -0,0 +1,153 @@
+export default [
+  {
+    id: 1,
+    title: "capitalismo digital",
+    radius: 175,
+    related: [5, 6, 7, 8],
+    position: {
+      x: 560,
+      y: 160,
+    },
+  },
+  {
+    title: "tecnologías alternativas",
+    radius: 120,
+    id: 2,
+    related: [3, 4],
+    position: {
+      x: 1100,
+      y: 290,
+    },
+  },
+  {
+    title: "apropiación tecnológica",
+    radius: 120,
+    id: 3,
+    related: [2, 4],
+    position: {
+      x: 900,
+      y: 550,
+    },
+  },
+  {
+    title: "tecnociencia feminista",
+    radius: 120,
+    id: 4,
+    related: [],
+    position: {
+      x: 1220,
+      y: 615,
+    },
+  },
+  {
+    title: "precariza",
+    radius: 50,
+    id: 5,
+    related: [1],
+    position: {
+      x: 290,
+      y: 120,
+    },
+  },
+  {
+    title: "alimenta lógicas consumistas",
+    radius: 50,
+    id: 6,
+    related: [1],
+    position: {
+      x: 380,
+      y: 280,
+    },
+  },
+  {
+    title: "destruye la vida",
+    radius: 50,
+    id: 7,
+    related: [1],
+    position: {
+      x: 180,
+      y: 360,
+    },
+  },
+  {
+    title: "destruye el planeta",
+    radius: 50,
+    id: 8,
+    related: [1],
+    position: {
+      x: 340,
+      y: 480,
+    },
+  },
+  {
+    title: "accesible",
+    radius: 50,
+    id: 9,
+    related: [],
+    position: {
+      x: 1560,
+      y: 120,
+    },
+  },
+  {
+    title: "de código abierto",
+    radius: 50,
+    id: 10,
+    related: [],
+    position: {
+      x: 380,
+      y: 280,
+    },
+  },
+  {
+    title: "con software libre",
+    radius: 50,
+    id: 11,
+    related: [],
+    position: {
+      x: 1420,
+      y: 270,
+    },
+  },
+  {
+    title: "recupera tecnologías antiguas",
+    radius: 50,
+    id: 12,
+    related: [],
+    position: {
+      x: 1650,
+      y: 340,
+    },
+  },
+  {
+    title: "autogestionada",
+    radius: 50,
+    id: 13,
+    related: [],
+    position: {
+      x: 1500,
+      y: 450,
+    },
+  },
+  {
+    title:
+      "con datos sólo explotables dentro de las comunidados que los generan",
+    radius: 50,
+    id: 14,
+    related: [],
+    position: {
+      x: 1620,
+      y: 620,
+    },
+  },
+  {
+    title: "que cuida las infraestructuras",
+    radius: 50,
+    id: 15,
+    related: [],
+    position: {
+      x: 1530,
+      y: 820,
+    },
+  },
+];
