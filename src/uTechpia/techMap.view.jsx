e63aa06a29f4d0cf57c800fe827e9cd509b9796b
Carla Rodriguez <n298775@gruposantander.com>
2020-11-13 17:31:01 +0100

    create-project-structure

---
+++ src/uTechpia/techMap.view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TechMapView = () => {
+  const title = '<uTECHpia>';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{title}</h1>
+      </header>
+    </div>
+  );
+};
+
+export default TechMapView;
