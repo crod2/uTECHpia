b5e36129a046b379ec5d44d7fe270c0dffda4ca9
Carla Rodriguez <n298775@gruposantander.com>
2020-11-20 17:09:10 +0100

    create story text

--- src/uTechpia/components/uTechStory.component.jsx
+++ src/uTechpia/components/uTechStory.component.jsx
@@ -4,7 +4,8 @@ import { NavLink } from 'react-router-dom';
 import { WrapperStyled, WrapperContainerStyled } from '../../sharedStyles.styled.js';
 
 const UTechStory = () => {
-  const story = '<story>';
+  const story =
+    'Este proyecto presenta algunas de las posibilidades que nos ofrecería la tecnología actual de escoger aplicarle la perspectiva feminista. Está inspirado en los debates que se han generado en el curso "Entendiendo el capitalismo digital y construyendo una crítica feminista" impartido por Marga Padilla + Otro Tiempo. Es la historia de la distopía que generaría el futurible colapso de las redes tecnológicas tal cual las conocemos, ligadas profundamente a los intereses del mercado. Quizás pueda ser, a través de la reflexión, de la crítica, del análisis, algo distinto. Nuestra historia.';
   return (
     <WrapperStyled>
       <WrapperContainerStyled>
