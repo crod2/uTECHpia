51a6bc09ab8a4b6ae1480189523e522b3149bc11
Carla Rodriguez <n298775@gruposantander.com>
2020-11-23 19:01:22 +0100

    haciendo pruebas

--- src/uTechpia/uTechpia.view.jsx
+++ src/uTechpia/uTechpia.view.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import UTechTitle from './components/titlePage/uTechTitle.component';
 
 const UTechpiaView = () => {
+  console.log('ale esta crazy');
   return <UTechTitle />;
 };
 
